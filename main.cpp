697ff09e29450ab00ad89e1353cdfcfb7b89b3f6
Benjo <benji06199@gmail.com>
2021-05-31 14:21:47 -0500

    Se agregaron los diferentes recorridos, busqueda de dato mayor y menor, y la funcion pde eliminacion de nodos

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <cstdlib>
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -10,7 +9,14 @@ typedef struct nodo {
 
 Nodo* Nuevo();
 Nodo* Insertar(int, Nodo*);
-bool Buscar(int dato, Nodo*);
+Nodo* Buscar(int dato, Nodo*);
+Nodo* BuscarMayor(Nodo*);
+Nodo* BuscarMenor(Nodo*);
+Nodo* Suprimir(int, Nodo*);
+Nodo* Cambiar(Nodo*, Nodo*);
+void VerPostOrden(Nodo*);
+void VerEnOrden(Nodo*);
+void VerPreOrden(Nodo*);
 void Error();
 void Menu();
 
@@ -29,19 +35,111 @@ int main(){
 				system("clear");
 				printf("Ingrese el dato a guardar:\t");
 				scanf("%d", &num);
-				p = Insertar(num, p);
+
+				if (Buscar(num, p) == NULL) {
+					p = Insertar(num, p);
+				}else {
+					printf("El dato ya esta en el arbol\n");
+				}
+
+				getchar();
 				getchar();
 				break;
 			case 2: 
 				system("clear");
 				printf("Ingrese el dato a buscar:\t");
 				scanf("%d", &num);
-				Buscar(num, p);
+
+				if (Buscar(num, p) != NULL){
+					printf("El dato %d, se a encontrado\n", Buscar(num, p)->dato); 
+				}else {
+					printf("No se ha encontrado el dato %d\n", num);
+				}
+
+				getchar();
+				getchar();
+				break;
+			case 3:
+				system("clear");
+				printf("Recorrido del arbol en Postorden\n");
+
+				if (p != NULL) {
+					VerPostOrden(p);
+				}else {
+					printf("El arbol esta vacio\n");
+				}
+
+				getchar();
+				getchar();
+				break;
+			case 4:
+				system("clear");
+				printf("Recorrido del arbol en Orden\n");
+	
+				if (p != NULL) {
+					VerEnOrden(p);
+				}else {
+					printf("El arbol esta vacio\n");
+				}			
+
+				getchar();
+				getchar();
+				break;
+			case 5:
+				system("clear");
+				printf("Recorrido del arbol en Preorden\n");
+				
+				if (p != NULL) {
+					VerPreOrden(p);
+				}else {
+					printf("El arbol esta vacio\n");
+				}			
+
+				getchar();
+				getchar();
+				break;
+			case 6:
+				system("clear");
+				
+				if (p != NULL) {
+					printf("El dato mayor es: %d\n", BuscarMayor(p)->dato);
+				}else {
+					printf("El arbol esta vacio\n");
+				}			
+
+				getchar();
+				getchar();
+				break;
+			case 7:
+				system("clear");
+
+				if (p != NULL) {
+					printf("El dato menor es: %d", BuscarMenor(p)->dato);
+				}else {
+					printf("El arbol esta vacio\n");
+				}			
+
+				getchar();
+				getchar();
+				break;
+			case 8:
+				system("clear");
+				printf("Inserte el dato a eliminar: ");
+				scanf("%d", &num);
+
+				if (Buscar(num, p) != NULL){
+					p = Suprimir(num, p);
+					printf("Dato Eliminado\n");
+				}else {
+					printf("El dato %d no esta guardado\n", num);
+				}
+
+				getchar();
 				getchar();
 				break;
 		}
 
-	}while (opc != 3);
+	}while (opc != 9);
 	
 	return 0;
 }
@@ -64,8 +162,7 @@ Nodo* Insertar(int dato, Nodo *p){
 		p->derecho = NULL;
 		p->izquierdo = NULL;
 		p->dato = dato;
-		printf("Se a guardado el dato\n");
-		getchar();
+		printf("Dato guardado");
 		return (p);
 	}
 
@@ -78,17 +175,13 @@ Nodo* Insertar(int dato, Nodo *p){
 	return (p);
 }
 
-bool Buscar(int dato, Nodo* p){
+Nodo* Buscar(int dato, Nodo* p){
 	if(p == NULL){
-		printf("Dato no encontrado\n");
-		getchar();
-		return false;
+		return NULL;
 	}
 
 	if(dato == p->dato){
-		printf("El dato se encuentra en el arbol\n");
-		getchar();
-		return true;
+		return p;
 	}
 
 	if (dato < p->dato) {
@@ -98,9 +191,100 @@ bool Buscar(int dato, Nodo* p){
 	}
 }
 
+void VerPostOrden(Nodo* p){
+	if (p == NULL) {
+		return;
+	}
+
+	VerPostOrden(p->izquierdo);
+	VerPostOrden(p->derecho);
+
+	printf("%d ", p->dato);
+}
+
+void VerEnOrden(Nodo* p){
+	if (p == NULL) {
+		return;
+	}
+
+	VerEnOrden(p->izquierdo);
+	printf("%d ", p->dato);
+	VerEnOrden(p->derecho);
+}
+
+void VerPreOrden(Nodo* p){
+	if (p == NULL) {
+		return;
+	}
+
+	printf("%d ", p->dato);
+	VerPreOrden(p->izquierdo);
+	VerPreOrden(p->derecho);
+}
+
+Nodo* BuscarMayor(Nodo* p){
+	if (p->derecho == NULL) {
+		return p;
+	}
+
+	return BuscarMayor(p->derecho);
+}
+
+Nodo* BuscarMenor(Nodo* p){
+	if (p->izquierdo == NULL) {
+		return p;
+	}
+
+	return BuscarMenor(p->izquierdo);
+}
+
+Nodo* Suprimir(int dato, Nodo* p){
+	if (p == NULL) {
+		return NULL;
+	}
+
+	if (dato < p->dato) {
+		p->izquierdo = Suprimir(dato, p->izquierdo);
+	}
+
+	if (dato > p->dato) {
+		p->derecho = Suprimir(dato, p->derecho);
+	}
+
+	if (p->dato == dato) {
+		if (p->derecho == NULL) {
+			p = p->izquierdo;
+		}else if (p->izquierdo == NULL) {
+			p = p->derecho;
+		}else {
+			p->derecho = Cambiar(p, p->derecho);
+		}
+	}
+
+	return p;
+}
+
+Nodo* Cambiar(Nodo* p, Nodo* s){
+	if (s->izquierdo == NULL) {
+		p->dato = s->dato;
+		s = s->derecho;
+		return s;
+	}
+
+	s->izquierdo = Cambiar(p, s->izquierdo);
+
+	return s;
+}
+
 void Menu(){
 	printf("Escoja una opcion\n");
 	printf("1.\tIngresar un dato\n");
 	printf("2.\tBuscar un dato\n");
-	printf("3.\tSalir\n");
+	printf("3.\tRecorrer el arbol en Postorden\n");
+	printf("4.\tRecorrer el arbol en Orden\n");
+	printf("5.\tRecorrer el arbol en Preorden\n");
+	printf("6.\tBuscar dato mas grande\n");
+	printf("7.\tBuscar el dato mas pequeño\n");
+	printf("8.\tSuprimir un dato\n");
+	printf("9.\tSalir\n");
 }
